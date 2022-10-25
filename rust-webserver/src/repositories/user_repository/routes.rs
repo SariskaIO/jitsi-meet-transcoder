9f6e6455ef62b7289dbea2c2adccdf63eb77456d
brajendrak00068 <brajendra@Brajendras-MacBook-Air-M1.local>
2022-10-25 19:45:19 +0530

    fix borrower checker issue

--- rust-webserver/src/repositories/user_repository/routes.rs
+++ rust-webserver/src/repositories/user_repository/routes.rs
@@ -1,6 +1,7 @@
 #![feature(libc)]
 extern crate libc;
-
+extern crate strfmt;
+use strfmt::strfmt;
 use std::env;
 use actix_web::{post, get, web, HttpRequest, HttpResponse};
 use serde::{Deserialize, Serialize};
@@ -9,21 +10,35 @@ use actix_web::{http::header::ContentType};
 use futures::future::{err, ok, Ready};
 use actix_web::error::ErrorUnauthorized;
 use std::process::Command;
-static GST_MEET_PARAMS_AUDIO_AND_VIDEO: &str = "../streaming-service-bridge/target/debug/gst-meet --web-socket-url=wss://api.sariska.io/api/v1/media/websocket  --xmpp-domain=sariska.io  --muc-domain=muc.sariska.io  --room-name=roomname     --recv-pipeline='compositor name=video ! videoconvert ! queue ! x264enc ! mpegtsmux ! filesink location=testvideo.mp4'";
-static GST_MEET_PARAMS_LIVESTREAM: &str = "./gst-meet --web-socket-url=wss://api.sariska.io/api/v1/media/websocket \
+use std::time::{SystemTime, UNIX_EPOCH};
+use rand::distributions::{Alphanumeric, DistString};
+use reqwest::header::{HeaderMap, HeaderName, HeaderValue, USER_AGENT, CONTENT_TYPE};
+use time::OffsetDateTime;
+
+// need to change this later when load balancer giving all correct IP's
+static RTMP_OUT_LOCATION: &str = "rtmp://43.205.21.202:1935/{app}/{stream}";
+
+static DASH_URL: &str = "https://edge.sariska.io/play/dash/{app}/{stream}.mpd";
+static HLS_URL: &str = "https://edge.sariska.io/play/hls/{app}/{stream}.m3u8";
+static MP3_URL: &str = "https://edge.sariska.io/play/dash/{app}/{stream}.mp3";
+static AAC_URL: &str = "https://edge.sariska.io/play/dash/{app}/{stream}.aac";
+static RTMP_URL: &str = "rtmp://a0f32a67911bd43b08097a2a99e6eac6-b0099fdbb77fd73a.elb.ap-south-1.amazonaws.com:1935/{app}/{stream}";
+static FLV_URL: &str = "https://edge.sariska.io/play/dash/{app}/{stream}.flv";
+
+static GST_MEET_PARAMS_LIVESTREAM: &str = "./gst-meet --web-socket-url=wss://api.sariska.
+o/api/v1/media/websocket \
 --xmpp-domain=sariska.io  --muc-domain=muc.sariska.io \
  --recv-video-scale-width=640 \
  --recv-video-scale-height=360 \
- --room-name=roomname  \
+ --room-name={roomname}  \
  --recv-pipeline='compositor name=video sink_1::xpos=640 \
     ! queue \
     ! x264enc cabac=1 bframes=2 ref=1 \
     ! video/x-h264,profile=main \
     ! flvmux streamable=true name=mux \
-    ! rtmpsink location=rtmp://43.205.21.202:1935/app/jitsistream \
+    ! rtmpsink location={rtmp_out_location} \
     audiotestsrc is-live=1 wave=ticks \
        ! mux.'";
-
 use std::process::Child;
 use std::sync::Mutex;
 use std::{collections::HashMap, pin::Pin, sync::RwLock};
@@ -63,89 +78,152 @@ fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
 }
 
+#[derive(Serialize)]
+struct ResponseStart {
+    started: bool,
+    hls_url:  String,
+    dash_url: String,
+    mp3_url: String,
+    aac_url: String,
+    rtmp_url: String,
+    flv_url: String
+}
+
+#[derive(Serialize)]
+struct ResponseStop {
+    stopped: bool
+}
+
+fn systemtime_strftime<T>(dt: T, format: &str) -> String
+   where T: Into<OffsetDateTime>
+{
+    dt.into().format(format)
+}
+
+
+async fn sendDataToPricingService(room_name: String, action: String, authorization_header: String) {
+    let mut map = HashMap::new();
+    let st = SystemTime::now();
+    let st_str: String= format!("{}", systemtime_strftime(st, "%d/%m/%Y %T"));
+    map.insert("roomJid", format!("{}@muc.sariska.io", room_name));
+    map.insert("timestamp",  st_str);
+    map.insert("action", action);
+    map.insert("type", "stream".to_owned());            
+
+    let serviceSecretKey = match env::var_os("X-SERVICE-TOKEN") {
+        Some(v) => v.into_string().unwrap(),
+        None => panic!("$X-SERVICE-TOKEN is not set")
+    };
+
+    let mut headers = HeaderMap::new();
+        headers.insert("Authorization", authorization_header.parse().unwrap());
+        headers.insert("X-SERVICE-TOKEN", serviceSecretKey.parse().unwrap());
+
+    let client = reqwest::Client::new();
+    let res = client.post("https://api.sariska.io/api/v1/pricing/recording")
+        .headers(headers)
+        .json(&map)
+        .send()
+        .await;
+}
+
 #[get("/startRecording")]
 async fn start_recorging(_req: HttpRequest, child_processes: web::Data<RwLock<AppState>>) -> HttpResponse {
     let params = web::Query::<Params>::from_query(_req.query_string()).unwrap();
     println!("{:?}", params);
 
-    
-//     let _auth = _req.headers().get("Authorization");
-//     let _split: Vec<&str> = _auth.unwrap().to_str().unwrap().split("Bearer").collect();
-//     let token = _split[1].trim();
-//     let header  =  decode_header(&token);
-//     let request_url = env::var("SECRET_MANAGEMENT_SERVICE_PUBLIC_KEY_URL").unwrap_or("none".to_string());  
-//     let kid = "";
+    let app: String =  Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
+    let stream: String =  Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
+    let location = format!("{}",strfmt!(RTMP_OUT_LOCATION, app, stream).unwrap());
+    let pipeline = format!("{}",strfmt!(GST_MEET_PARAMS_LIVESTREAM, params.room_name, location).unwrap());
+    let _auth = _req.headers().get("Authorization");
+    let _split: Vec<&str> = _auth.unwrap().to_str().unwrap().split("Bearer").collect();
+    let token = _split[1].trim();
+    let header  =  decode_header(&token);
+    let request_url = env::var("SECRET_MANAGEMENT_SERVICE_PUBLIC_KEY_URL").unwrap_or("none".to_string());  
+    let kid = "";
 
-//    let headerData = match header {
-//         Ok(_token) => _token.kid,
-//         Err(_e) => None,
-//     };
-//     let kid = headerData.as_deref().unwrap_or("default string");
-//         // create a Sha256 object
-//     let api_key_url =  format!("{}/{}", request_url, kid);
-//     let response = reqwest::get(api_key_url)
-//         .await
-//         .unwrap()
-//         .text()
-//         .await;
-//     let publicKey  = match response {
-//         Ok(_publickey)=>_publickey,
-//         _ => "default string".to_string(),
-//     };
+   let headerData = match header {
+        Ok(_token) => _token.kid,
+        Err(_e) => None,
+    };
+    let kid = headerData.as_deref().unwrap_or("default string");
+        // create a Sha256 object
+    let api_key_url =  format!("{}/{}", request_url, kid);
+    let response = reqwest::get(api_key_url)
+        .await
+        .unwrap()
+        .text()
+        .await;
+    let publicKey  = match response {
+        Ok(_publickey)=>_publickey,
+        _ => "default string".to_string(),
+    };
     
-//     let deserialized: PublicKey = serde_json::from_str(&publicKey).unwrap();
-//     let decoded_claims = decode::<Claims>(
-//         &token,
-//         &DecodingKey::from_rsa_components(&"deserialized.n", &deserialized.e),
-//         &Validation::new(Algorithm::RS256));
+    let deserialized: PublicKey = serde_json::from_str(&publicKey).unwrap();
+    let decoded_claims = decode::<Claims>(
+        &token,
+        &DecodingKey::from_rsa_components(&"deserialized.n", &deserialized.e),
+        &Validation::new(Algorithm::RS256));
 
-//         let decoded;
-//         let mut error = false;
-//         match decoded_claims {
-//             Ok(v) => {
-//                 decoded = v;
-//             },
-//             Err(e) => {
-//               error = true;
-//             },
-//         }
+        let decoded;
+        let mut error = false;
+        match decoded_claims {
+            Ok(v) => {
+                decoded = v;
+            },
+            Err(e) => {
+              error = true;
+            },
+        }
         
-//         if error == true {
-//             println!("unauthorized");
-//             return HttpResponse::Unauthorized().json("{}");
-//         }
+        if error == true {
+            println!("unauthorized");
+            return HttpResponse::Unauthorized().json("{}");
+        }
 
-
-        // let output = Command::new("sh")
-        // .spawn()
-        // .expect("failed to execute process");
         let child = Command::new("sh")
         .arg("-c")
         .arg(GST_MEET_PARAMS_LIVESTREAM)
         .spawn()
         .expect("failed to execute process");
          child_processes.write().unwrap().map.insert(params.room_name.to_string(), child.id().to_string());
-
         // child_processes.insert(params.room_name.to_string(),child);
-        // let hello = output.stdout;
 
-        // println!("qwnenqwnenwq enqwne nqwen {:?}", output);
-
+        sendDataToPricingService(params.room_name, "start".to_owned(), token.to_owned());
         
+        let obj = ResponseStart {
+            started: true,
+            hls_url: format!("{}",strfmt!(HLS_URL, app, stream).unwrap()),
+            dash_url: format!("{}",strfmt!(DASH_URL, app, stream).unwrap()),
+            mp3_url: format!("{}",strfmt!(MP3_URL, app, stream).unwrap()),
+            aac_url: format!("{}",strfmt!(AAC_URL, app, stream).unwrap()),
+            rtmp_url: format!("{}",strfmt!(RTMP_URL, app, stream).unwrap()),
+            flv_url: format!("{}",strfmt!(FLV_URL, app, stream).unwrap())
+        };
+        HttpResponse::Ok().json(obj)
 
-        HttpResponse::Ok().json("{}")
 }
 
 #[get("/stopRecording")]
 async fn stop_recording(_req: HttpRequest, child_processes: web::Data<RwLock<AppState>>) -> HttpResponse {
     let params = web::Query::<Params>::from_query(_req.query_string()).unwrap();
+    let _auth = _req.headers().get("Authorization");
+    let _split: Vec<&str> = _auth.unwrap().to_str().unwrap().split("Bearer").collect();
+    let token = _split[1].trim();
+
     let mut child_ids = &child_processes.read().unwrap().map;
     let child_os_id = child_ids.get(&params.room_name.to_string());  
     let my_int = child_os_id.unwrap().parse::<i32>().unwrap();
     unsafe {
         kill(my_int, SIGTERM);
     }
-    HttpResponse::Ok().json("{}")
+    sendDataToPricingService(params.room_name, "stop".to_owned(), token.to_owned());
+    
+    let obj = ResponseStop {
+        stopped: true,
+    };
+    HttpResponse::Ok().json(obj)
 }
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
